1d2fcca1855754fa278a3e581e949f9e8e695d0d
Nha Hoang <nhatuhoang91@gmail.com>
2023-10-25 22:33:01 -0500

    working on mobile-nav

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -1,1 +1,65 @@
 'use client'
+
+import Link, {LinkProps } from 'next/link'
+import { useRouter } from 'next/router'
+import * as React from 'react'
+import {Menu} from 'lucide-react'
+
+import { cn } from '@/lib/utils'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+import { Button } from './ui/button'
+
+
+export const MobileNav = () => {
+    const [open, setOpen] = React.useState(false)
+
+    return(
+        <Sheet open={open} onOpenChange={setOpen}>
+            <SheetTrigger asChild>
+                <Button variant='ghost'
+                    className='mr-2 px-0 text-base hover:bg-transparent
+                        focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0
+                        md:hidden'
+                >
+                    <Menu className='h-5 w-5'/>
+                    <span className='sr-only'>Toggle Menu</span>
+                </Button>
+            </SheetTrigger>
+            <SheetContent side='left' className='pr-0'>
+                <MobileLink href='/'
+                    onOpenChange={setOpen}>
+                    <span className='font-bold'>wuDeal</span>
+                </MobileLink>
+            </SheetContent>
+        </Sheet>
+    )
+}
+
+interface MobileLinkProps extends LinkProps{
+    onOpenChange? : (open : boolean) => void
+    children : React.ReactNode
+    className? : string
+}
+
+function MobileLink ({
+    href,
+    onOpenChange,
+    className,
+    children,
+    ...props
+} : MobileLinkProps){
+    const router = useRouter()
+    return (
+        <Link
+            href={href}
+            onClick={()=>{
+                router.push(href.toString())
+                onOpenChange?.(false)
+            }}
+            className={cn(className)}
+            {...props}>
+                {children}
+        </Link>
+    )
+}
+
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,10 +1,12 @@
 import { MainNav } from "@/components/main-nav"
+import { MobileNav } from "./mobile-nav"
 
 export const SiteHeader = () => {
     return (
         <header className="sticky top-0 z-50 w-ful bg-background/50 backdrop-blur">
-            <div className="container flex h-14 items-center bg-purple-100"> 
+            <div className="container flex h-14 items-center"> 
                 <MainNav/>
+                <MobileNav/>
             </div>
         </header>
     )
